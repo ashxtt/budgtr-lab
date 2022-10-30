67a82dcfe13620d79388ec8c83e56b1fd431e83e
Ash Grey <111319560+ashxtt@users.noreply.github.com>
2022-10-29 17:46:38 -0700

    Add files via upload
    Final update

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
-const budget = require('./models/budget')
+const budget = require('./models/budget.js')
 const bodyParser = require('body-parser')
 
 const port = 8000
-
+//MIDLEWARE
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: false}))
-//MIDLEWARE
+
 
 
 app.get('/',(req, res)=>{
-    res.render('index.ejs', {budgets: budget});
+    res.render('index.ejs', {budget: budget});
 })
 
 app.get('/show/:id', (req, res)=>{
@@ -23,9 +23,10 @@ app.get('/show/:id', (req, res)=>{
 app.get('/new', (req, res) => {
     res.render('new.ejs');
 })
+
 app.post('/new', (req, res)=>{
     new_budget = req.body;
-    Budgets.push(new_budget);
+    budget.push(new_budget);
     res.redirect('/')
 })
 
